560124ee1ecc1a39e0d14e31dd69caf272c8c023
keyrun-14 <tamvadakirankumar007gmail.com>
2022-04-09 16:26:18 +0530

    assignment-4 submission

--- Assignment-4/code/src/index.js
+++ Assignment-4/code/src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Postview from './Postview';
 import reportWebVitals from './reportWebVitals';
-
-
-
-
-
 ReactDOM.render(
   <React.StrictMode>
     <Postview />
